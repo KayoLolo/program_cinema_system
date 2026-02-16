ef68b118e58c20077879bec965d1a1d6bf956b1e
karlcreppy@gmail.com <karlcreppy@gmail.com>
2026-02-16 22:08:13 +0100

    Test with class MovieSlotTestData

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args){
+        MovieScheduler movieScheduler = new MovieScheduler();
+
+        System.out.println("===  ===");
+        movieScheduler.importMovies(MovieSlotTestData.getDuplicateMovieSchedule());
+        System.out.println("=== ===");
+        movieScheduler.importMovies(MovieSlotTestData.getProblematicMovieSchedule());
+        System.out.println("=== ===");
+        movieScheduler.importMovies(MovieSlotTestData.getDuplicateMovieSchedule());
+        System.out.println("=== ===");
+        movieScheduler.importMovies(MovieSlotTestData.getConflictingSchedule());
+        System.out.println("=== ===");
+        movieScheduler.importMovies(MovieSlotTestData.getInvalidMovieSchedule());
+    }
+}
